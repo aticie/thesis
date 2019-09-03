452c55d001c282677cf6736ee84c0f3968b04ff3
Efehan Atıcı <e.atici@vispera.co>
2019-09-03 11:34:23 +0300

    multithread added

---
+++ cpp2/read_vid.py
@@ -0,0 +1,158 @@
+from __future__ import division
+import operator
+import numpy as np
+import cv2
+import math
+from threading import Thread
+import threading
+import sys
+from Queue import Queue
+from StreamClass import FileVideoStream
+from util import *
+import time
+
+def click_and_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global clicked_point, held_point
+ 
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        clicked_point = (x,y)
+        
+    if flags & cv2.EVENT_FLAG_LBUTTON == 1:
+        if event == cv2.EVENT_MOUSEMOVE:
+            held_point = (x,y)
+
+vid_name = 'big_buck_bunny_480p_1mb.mp4'
+
+clicked_point = (0,0)
+held_point = (0,0)
+state = 'Play'
+cv2.namedWindow("frame")
+cv2.setMouseCallback("frame", click_and_crop)
+crop_start = 0
+crop_end = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+fvs = FileVideoStream(vid_name).start()
+length = fvs.length
+time.sleep(1.0)
+i=0
+
+while True:
+    if state == 'Quit':
+        break
+    # Capture frame-by-frame
+    if state == 'Play':
+        while True:
+
+            frame = fvs.read()
+            
+            h,w,c = frame.shape
+            frame = add_buttons(frame, fvs.curFrame, length)
+            frame = add_current_frame_text(frame, fvs.curFrame, length, font)
+            frame = add_crop_frame(frame, crop_start, crop_end)
+            frame = cv2.rectangle(frame, (340, 400), (400,460), (12, 30, 255), 2)
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            cv2.imshow('frame', frame)    
+            key = cv2.waitKey(30) & 0xFF
+
+            
+
+            # Check if click is on seeking bar (and seek video)
+            if tuple_compare(clicked_point,(40,h-100),operator.gt) and tuple_compare(clicked_point,(w-40,h-80),operator.lt):
+                fvs.curFrame = int((clicked_point[0]-40)/(w-80)*length)
+                state = "Pause"
+                break
+
+            # Check if click is on pause button
+            clicked_point_on_pause = tuple_compare(clicked_point,(240,400),operator.gt) and tuple_compare(clicked_point,(300,460),operator.lt)
+            if key == ord(' ') or key == ord('c') or clicked_point_on_pause:
+                clicked_point = (0,0)
+                state = 'Pause'
+                break
+
+            if cv2.getWindowProperty('frame', 0) < 0:     
+                state = "Quit"   
+                break
+
+    elif state == 'Pause':
+
+        frame = fvs.read()
+        frame = add_buttons(frame, fvs.curFrame, length)
+        frame = add_current_frame_text(frame, fvs.curFrame, length, font)
+        frame = add_crop_frame(frame, crop_start, crop_end)
+        frame = cv2.rectangle(frame, (240, 400), (300,460), (12, 30, 255), 2)
+        while(True):
+
+            h,w,c = frame.shape
+
+            cv2.imshow('frame', frame)    
+            key = cv2.waitKey(1) & 0xFF
+
+            clicked_point_on_resume = tuple_compare(clicked_point,(340,400),operator.gt) and tuple_compare(clicked_point,(400,460),operator.lt)
+            clicked_point_on_fwd_frame = tuple_compare(clicked_point,(440,400),operator.gt) and tuple_compare(clicked_point,(500,460),operator.lt)
+            clicked_point_on_rwd_frame = tuple_compare(clicked_point,(140,400),operator.gt) and tuple_compare(clicked_point,(200,460),operator.lt)
+            if key == ord(' ') or key == ord('v') or clicked_point_on_resume:
+                clicked_point = (0,0)
+                state = 'Play'
+                break
+
+            elif key == ord('x') or clicked_point_on_rwd_frame:
+                clicked_point = (0,0)
+                state = 'Rwd-frame'
+                break
+
+            elif key == ord('b') or clicked_point_on_fwd_frame:
+                clicked_point = (0,0)
+                state = 'Fwd-frame'
+                break
+
+            if key == ord('o'):
+                crop_start = fvs.curFrame
+                frame = add_crop_frame(frame, crop_start, crop_end)
+
+
+            if key == ord('p'):
+                crop_end = fvs.curFrame
+                frame = add_crop_frame(frame, crop_start, crop_end)
+
+            if key == ord('r'):
+                crop_end = 0
+                crop_start = 0
+
+            if key == ord('l'):
+                crop_video_start_label(frame, crop_start, crop_end, fvs, vid_name)
+                create_and_save_json(crop_start, crop_end)
+
+            # Check if you held down on seek bar
+            # if tuple_compare(clicked_point,(40,h-100),operator.gt) and tuple_compare(clicked_point,(w-40,h-80),operator.lt):
+            #     fvs.curFrameNo = int((clicked_point[0]-40)/(w-80)*length)
+            #     fvs.seek = True
+            #     clicked_point = (0,0)
+            #     state = "Pause"
+            #     break
+
+            # if tuple_compare(held_point,(40,h-100),operator.gt) and tuple_compare(held_point,(w-40,h-80),operator.lt):
+            #     fvs.curFrameNo = int((held_point[0]-40)/(w-80)*length)
+            #     fvs.seek = True
+            #     held_point = (0,0)
+            #     state = "Pause"
+            #     break
+
+            if cv2.getWindowProperty('frame', 0) < 0:     
+                state = "Quit"   
+                break
+
+    elif state == 'Rwd-frame':
+        fvs.rwd_frame = True
+        state = 'Pause'
+
+    elif state == 'Fwd-frame':
+        state = 'Pause'
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ cpp2/util.py
@@ -0,0 +1,166 @@
+from __future__ import division
+import cv2
+import numpy as np
+import operator
+import math
+from StreamClass import FileVideoStream
+import os
+import json
+
+def add_buttons(frame, current_frame, length):
+
+    h, w, c = frame.shape
+    button_size = 60
+    m0 = 40
+    m1 = 20
+
+    for i in range(6):
+        button_pos_1 = (m0*(i+1)+button_size*i, h-m1)
+        button_pos_2 = ((m0+button_size)*(i+1), h-m1-button_size)
+        cv2.rectangle(frame, button_pos_1, button_pos_2, (53,53,53), -1)
+
+        top_left_x = button_pos_1[0]
+        top_left_y = button_pos_1[1]
+        # Rewind button
+        if i == 0:
+            # Rewind button
+            rewind_pts = [[m0+int(button_size/5), h-m1-int(button_size/2)], #mid point of left triangle
+                          [m0+int(button_size*3/5), h-m1-int(button_size/5)], #lower point of left triangle
+                          [m0+int(button_size*3/5), h-m1-int(button_size*2/5-3)], # lower point of intersection
+                          [m0+int(button_size*4/5), h-m1-int(button_size/5)], #lower point of right triangle
+                          [m0+int(button_size*4/5), h-m1-int(button_size*4/5)], #upper point of right triangle
+                          [m0+int(button_size*3/5), h-m1-int(button_size*3/5+3)], #upper point of intersection
+                          [m0+int(button_size*3/5), h-m1-int(button_size*4/5)]] #upper point of left triangle
+            pts = np.array(rewind_pts)
+            cv2.fillPoly(frame, np.int32([pts]),(255,255,255))
+
+        # Rewind by frame
+        if i == 1:
+            cv2.rectangle(frame, (top_left_x+int(button_size/5), top_left_y-int(button_size/5)),(top_left_x+int(button_size*3/10), top_left_y-int(button_size*4/5)), (255,255,255), -1)
+            tri_pts = [[top_left_x+int(button_size*3/10),top_left_y-int(button_size/2)],
+                       [top_left_x+int(button_size*4/5), top_left_y-int(button_size/5)],
+                       [top_left_x+int(button_size*4/5), top_left_y-int(button_size*4/5)]]
+            cv2.fillPoly(frame, np.int32([tri_pts]), (255,255,255))
+        
+        #Pause button
+        if i == 2:
+            cv2.rectangle(frame, (top_left_x+int(button_size/5),top_left_y-int(button_size*4/5)), (top_left_x+int(button_size*2/5),top_left_y-int(button_size*1/5)), (255,255,255), -1)
+            cv2.rectangle(frame, (top_left_x+int(button_size*3/5),top_left_y-int(button_size*4/5)), (top_left_x+int(button_size*4/5),top_left_y-int(button_size*1/5)), (255,255,255), -1)
+
+        # Resume button
+        if i == 3:
+            tri_pts = [[top_left_x+int(button_size*4/5), top_left_y-int(button_size/2)], #mid point of triangle
+                       [top_left_x+int(button_size*1/5), top_left_y-int(button_size*4/5)], #lower point of triangle
+                       [top_left_x+int(button_size*1/5), top_left_y-int(button_size*1/5)]]
+            cv2.fillPoly(frame, np.int32([tri_pts]), (255,255,255))
+
+        # Forward by frame
+        if i == 4: 
+            cv2.rectangle(frame, (top_left_x+int(button_size*4/5), top_left_y-int(button_size/5)),(top_left_x+int(button_size*7/10), top_left_y-int(button_size*4/5)), (255,255,255), -1)
+            tri_pts = [[top_left_x+int(button_size*7/10), top_left_y-int(button_size/2)],
+                       [top_left_x+int(button_size*1/5), top_left_y-int(button_size/5)],
+                       [top_left_x+int(button_size*1/5), top_left_y-int(button_size*4/5)]]
+            cv2.fillPoly(frame, np.int32([tri_pts]), (255,255,255))
+
+        # Forward button
+        if i == 5:
+            rewind_pts = [[top_left_x+int(button_size*4/5), h-m1-int(button_size/2)], #mid point of left triangle
+                          [top_left_x+int(button_size*2/5), h-m1-int(button_size/5)], #lower point of left triangle
+                          [top_left_x+int(button_size*2/5), h-m1-int(button_size*2/5-3)], # lower point of intersection
+                          [top_left_x+int(button_size*1/5), h-m1-int(button_size/5)], #lower point of right triangle
+                          [top_left_x+int(button_size*1/5), h-m1-int(button_size*4/5)], #upper point of right triangle
+                          [top_left_x+int(button_size*2/5), h-m1-int(button_size*3/5+3)], #upper point of intersection
+                          [top_left_x+int(button_size*2/5), h-m1-int(button_size*4/5)]] #upper point of left triangle
+            pts = np.array(rewind_pts)
+            cv2.fillPoly(frame, np.int32([pts]),(255,255,255))
+
+    seeker_bar_top_left = (m0, h-(button_size+m1+10))
+    seeker_bar_bottom_right= (w-m0, h-(button_size+m1+15))
+    seeker_pos = int((current_frame/length)*(w-m0*2)+m0)
+    seeker_tl = (seeker_pos, seeker_bar_top_left[1]-10)
+    seeker_br = (seeker_pos+5, seeker_bar_bottom_right[1]+10)
+    cv2.rectangle(frame, seeker_bar_top_left, seeker_bar_bottom_right, (53,53,53), -1)
+    cv2.rectangle(frame, seeker_tl, seeker_br, (23,10,255), -1)
+
+    return frame
+
+def tuple_compare(tup1, tup2, relate):
+
+    for i,j in zip(tup1, tup2):
+        if not relate(i,j):
+            return False
+
+    return True
+
+
+def add_current_frame_text(frame, current_frame, length, font):
+    h,w,c = frame.shape
+    cv2.putText(frame,str(current_frame)+"/"+str(length),(0,20), font, 0.8 ,(53,33,255),1,cv2.LINE_AA)
+
+    return frame
+
+def add_crop_frame(frame, crop_start, crop_end):
+    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    if crop_start != 0:
+        cv2.putText(frame,"Crop start: "+str(crop_start+1),(0,50), font, 0.8 ,(53,33,255),1,cv2.LINE_AA)
+    if crop_end != 0:
+        cv2.putText(frame,"Crop end: "+str(crop_end+1),(0,80), font, 0.8 ,(53,33,255),1,cv2.LINE_AA)
+
+    return frame
+
+
+def crop_video_start_label(frame, crop_start, crop_end, fvs, vid_name):
+
+    print("Saving...")
+    frame_width = fvs.stream.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frame_height = fvs.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fps = fvs.stream.get(cv2.CAP_PROP_FPS)
+    fvs2 = FileVideoStream(vid_name).start()
+    output = cv2.VideoWriter(vid_name.split(".")[0]+"_out.avi", cv2.VideoWriter_fourcc('M','J','P','G'), int(fps), (int(frame_width),int(frame_height)))
+
+    for i in range(crop_start, crop_end):
+        fvs2.frameNo = i
+        save_frame = fvs2.read()
+        output.write(save_frame)
+
+    print("Saving Completed!")
+
+    return None
+
+def create_and_save_json(crop_start, crop_end):
+
+    try:
+        with open("interactions.json", "r") as f:
+            interactions = json.load(f)
+        latest = interactions["latest"]
+    except Exception as e:
+        print(e)
+        interactions = {}
+        latest = 0
+
+    latest += 1
+    print("Groups ?:")
+    groups = []
+
+    cur_split = {}
+    cur_split["start"] = crop_start
+    cur_split["end"] = crop_end
+
+    while True:
+        grp = raw_input()
+        if grp == "":
+            break
+        grp = grp.split(" ")
+        groups.append(grp)
+
+    cur_split["groups"] = groups
+
+    interactions["latest"] = latest
+    interactions[latest] = cur_split
+
+    with open("interactions.json", "w") as f:
+        json.dump(interactions, f, indent=2, sort_keys=True)
+
+    print("Saving interactions to interactions.json")
